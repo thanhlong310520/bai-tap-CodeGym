6250b1ec709d3a4da17a5237804fd327273d97a5
TLong <tilthanhlong@gmail.com>
2023-04-01 14:26:42 +0700

    bai2 thuc hanh su dung toan tu

---
+++ Bai2_ThucHanhSuDungToanTu/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Bai2_ThucHanhSuDungToanTu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float width;
+            float height;
+            Console.Write("Enter width: ");
+            width = float.Parse(Console.ReadLine());
+            Console.Write("Enter height: ");
+            height = float.Parse(Console.ReadLine());
+            float area = width * height;
+            Console.WriteLine("Width is: " + width);
+            Console.WriteLine("Height is: " + height);
+            Console.WriteLine("Area is: " + area);
+        }
+    }
+}
