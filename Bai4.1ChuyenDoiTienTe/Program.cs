45332948c64f1e046e078e74e47dc2d066c7c41a
TLong <tilthanhlong@gmail.com>
2023-04-03 15:20:57 +0700

    chuyen doi tien te

---
+++ Bai4.1ChuyenDoiTienTe/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Bai4._1ChuyenDoiTienTe
+{
+    class Program
+    {
+        const int tigia = 23000;
+        static void Main(string[] args)
+        {
+            Console.WriteLine("nhap so tien: ");
+            float usd = float.Parse(Console.ReadLine());
+            float vnd = usd * tigia;
+            Console.WriteLine("chuyen doi: " + vnd);
+        }
+    }
+}
