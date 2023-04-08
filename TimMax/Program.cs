b0da004a62af411b97cf8163d1aa758e6be98e7a
TLong <tilthanhlong@gmail.com>
2023-04-08 10:54:23 +0700

    Mang - Tim gia tri lon nhat trong mang 1 chieu

---
+++ TimMax/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace TimMax
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("nhap so phan tu mang: ");
+            int sptm = int.Parse(Console.ReadLine());
+            int[] arr = new int[sptm];
+
+            Console.WriteLine("nhap mang");
+            string[] inp = Console.ReadLine().Split();
+            for(int i = 0; i < sptm; i++)
+            {
+                arr[i] = Convert.ToInt32(inp[i]);
+            }
+            int max = arr[0];
+            foreach(var temp in arr)
+            {
+                if (max < temp)
+                    max = temp;
+            }
+            Console.WriteLine($"phan tu lon nhat trong mang la : {max}");
+        }
+    }
+}
