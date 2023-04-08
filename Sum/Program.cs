52cec535b8d6aaf3e7af55e414bcaadc03eb3ce8
TLong <tilthanhlong@gmail.com>
2023-04-08 10:46:37 +0700

    Mang - tinh tong cac phan tu mang

---
+++ Sum/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("nhap so  phan tu mang: ");
+            int sptm = Convert.ToInt32(Console.ReadLine());
+            int[] arr = new int[sptm];
+            Console.Write("nhap mang: ");
+            string[] inp = Console.ReadLine().Split();
+            
+            for (int i = 0; i < sptm; i++)
+            {
+                arr[i] = Convert.ToInt32(inp[i]);
+            }
+            int tong = 0;
+            foreach(var num in arr)
+            {
+                tong += num;
+            }
+            Console.WriteLine("tong: " + tong);
+        }
+    }
+}
