4808446bc820f0323029c177807f0ff07e0a2a8f
TLong <tilthanhlong@gmail.com>
2023-04-08 11:00:53 +0700

    Mang - tinh tong duong cheo chinh trong ma tran vuong

---
+++ Sum2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Sum2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("nhap hang: ");
+            int hang = Convert.ToInt32(Console.ReadLine());
+            float[,] arr2 = new float[hang, hang];
+            Console.WriteLine("nhap mang: ");
+            for (int i = 0; i < hang; i++)
+            {
+                string[] inp = Console.ReadLine().Split();
+                for (int k = 0; k < hang; k++)
+                {
+                    arr2[i, k] = float.Parse(inp[k]);
+                }
+            }
+            float tong = 0;
+            for (int i = 0; i < hang; i++)
+            {
+                tong += arr2[i, i];
+            }
+            Console.WriteLine("tong duong cheo chinh = " + tong);
+        }
+    }
+}
