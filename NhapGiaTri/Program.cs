273d7cca3266821b2e57fec1046bbf2b5e237229
TLong <tilthanhlong@gmail.com>
2023-04-08 10:56:59 +0700

    Mang - them phan tu vao mang

---
+++ NhapGiaTri/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace NhapGiaTri
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("nhap so  phan tu mang: ");
+            int sptm = Convert.ToInt32(Console.ReadLine());
+            int x;
+            int[] arr = new int[sptm];
+            do
+            {
+                Console.Write("nhap vi tri(lon hon 0 va nho hon size): ");
+                x = Convert.ToInt32(Console.ReadLine());
+            } while (x < 0 || x > sptm - 1);
+            Console.Write("nhap Gia tri: ");
+            int val = Convert.ToInt32(Console.ReadLine());
+            arr[x] = val;
+            foreach(var temp in arr)
+            {
+                Console.Write(temp + " ");
+            }
+        }
+    }
+}
