0c4333a4af454d70d8e96a98e19264bae24ba417
TLong <tilthanhlong@gmail.com>
2023-04-12 09:45:01 +0700

    Ham- tim gia tri nho nhat

---
+++ TimMin/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace TimMin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 4, 12, 7, 8, 1, 6, 9 };
+            int index = FindPosMin(arr);
+            Console.WriteLine("The smallest element in the array is: " + arr[index]);
+
+        }
+        static int FindPosMin(int[] arr)
+        {
+            int min = arr[0];
+            int index = 0;
+            for(int i = 0; i<arr.Length;i++)
+            {
+                if (min > arr[i])
+                {
+                    min = arr[i];
+                    index = i;
+                }
+            }
+            return index;
+        }
+    }
+}
