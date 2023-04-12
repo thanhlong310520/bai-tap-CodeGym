ae8f8d82132ce31d369147a2bc451349fe3b0f41
TLong <tilthanhlong@gmail.com>
2023-04-12 09:41:31 +0700

    Ham- chuyen doi nhiet do tu F sang C

---
+++ ChuyenDoiNhietDo/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace ChuyenDoiNhietDo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double fahrenheit;
+            double celsius;
+            int choice;
+
+            do
+            {
+                Console.WriteLine("Menu.");
+                Console.WriteLine("1. Fahrenheit to Celsius");
+                Console.WriteLine("2. Celsius to Fahrenheit");
+                Console.WriteLine("0. Exit");
+                Console.WriteLine("Enter your choice: ");
+                choice = Int32.Parse(Console.ReadLine());
+
+                switch (choice)
+                {
+                    case 1:
+                        Console.Write("Enter fahrenheit: ");
+                        fahrenheit = Convert.ToDouble(Console.ReadLine());
+                        Console.WriteLine("Fahrenheit to Celsius: " + FtoC(fahrenheit));
+                        break;
+                    case 2:
+                        Console.Write("Enter Celsius: ");
+                        celsius = Double.Parse(Console.ReadLine());
+                        Console.WriteLine("Celsius to Fahrenheit: " + CtoF(celsius));
+                        break;
+                    case 0:
+                        Environment.Exit(0);
+                        break;
+                }
+            } while (choice != 0);
+        }
+        static double CtoF(double celsius)
+        {
+            double fahrenheit = (9.0 / 5) * celsius + 32;
+            return fahrenheit;
+        }
+        static double FtoC(double fahrenheit)
+        {
+            double celsius = (5.0 / 9) * (fahrenheit - 32);
+            return celsius;
+        }
+    }
+}
