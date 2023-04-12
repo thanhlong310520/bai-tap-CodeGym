5bffa2db2b2bbd2e8b1d292d0ec732db4abdf673
TLong <tilthanhlong@gmail.com>
2023-04-12 09:43:44 +0700

    Ham- Dem so lan suat hien cua ki tu trong chuoi

---
+++ DemSoLanXuatHien/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace DemSoLanXuatHien
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("nhap chuoi: ");
+            string str = Console.ReadLine();
+            char kitu = (char)Console.Read();
+            Console.WriteLine($"So luong ki tu '{kitu}' co trong chuoi la: "+CountCharNumber(str,kitu));
+        }
+
+        static int CountCharNumber(string str,char c)
+        {
+            int count = 0;
+            foreach(char temp in str)
+            {
+                if(c == temp) count++;
+            }
+            return count;
+        }
+    }
+}
