4d132bc6a2580c807795dc1df959a596de21c671
TLong <tilthanhlong@gmail.com>
2023-04-27 08:32:24 +0700

    Trien khai interface IComparable cho cac lop hinh hoc

---
+++ Trien khai IComparable/ComperableCircle.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Trien_khai_IComparable
+{
+    class ComperableCircle : Circle,IComparable
+    {
+        public ComperableCircle(double radius) : base(radius)
+        {
+        }
+
+        public ComperableCircle(double radius, string color, bool filled) : base(radius, color, filled)
+        {
+        }
+
+        public int CompareTo(object obj)
+        {
+            ComperableCircle o = obj as ComperableCircle;
+            if (this.Radius > o.Radius)
+            {
+                return 1;
+            }
+            else if(this.Radius < o.Radius)
+            {
+                return -1;
+            }
+            return 1;
+        }
+    }
+}
