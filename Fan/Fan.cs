4b6ff80fcd14c5578d5a55964c3bd2e662c1aab2
TLong <tilthanhlong@gmail.com>
2023-04-27 08:16:16 +0700

    xay dung lop Fan

---
+++ Fan/Fan.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Fan
+{
+    class Fan
+    {
+        int speed = (int)SPEED.SLOW;
+        bool sttOn = false;
+        double radius = 5;
+        string color = "blue";
+
+        public int Speed { get => speed; set => speed = value; }
+        public bool SttOn { get => sttOn; set => sttOn = value; }
+        public double Radius { get => radius; set => radius = value; }
+        public string Color { get => color; set => color = value; }
+
+        public override string ToString()
+        {
+            if (sttOn)
+            {
+                return "speed = " + CheckSpeed() + ", color " + color + ", radius " + radius + ", fan is on";
+            }
+            else
+            {
+                return "color " + color + ", radius " + radius + ", fan is off"; 
+            }
+        }
+        SPEED CheckSpeed()
+        {
+            
+            if(speed == (int)SPEED.SLOW)
+            {
+                return SPEED.SLOW;
+            }
+            else if(speed == (int)SPEED.MEDIUM)
+            {
+                return SPEED.MEDIUM;
+            }
+            else if(speed == (int)SPEED.FAST)
+            {
+                return SPEED.FAST;
+            }
+            return SPEED.SLOW;
+            
+        }
+    }
+    public enum SPEED
+    {
+        SLOW = 1,MEDIUM,FAST
+    }
+}
---
+++ Fan/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Fan
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Fan f1 = new Fan();
+            f1.Speed = (int)SPEED.FAST;
+            f1.Radius = 10;
+            f1.Color = "yellow";
+            f1.SttOn = true;
+            Console.WriteLine("quat thu 2 la : " + f1.ToString());
+            Fan f2 = new Fan();
+            f2.Speed = (int)SPEED.MEDIUM;
+            f2.Radius = 5;
+            f2.SttOn = false;
+            Console.WriteLine("quat thu 2 la : " + f2.ToString());
+        }
+    }
+}
