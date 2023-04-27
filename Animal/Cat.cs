ed7b1158713ed11ff64ef9edf12afa67bcc11bc0
TLong <tilthanhlong@gmail.com>
2023-04-27 08:11:01 +0700

    xay dung class Animal

---
+++ Animal/Cat.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Animal
+{
+    class Cat : Animal
+    {
+        private string Name { get; set; }
+        public Cat(string weight, string height, string name)
+        : base(weight, height)
+        {
+            this.Name = name;
+        }
+
+        public override void PrintInfo()
+        {
+            Console.WriteLine("Weight : {0}" + '\n' + "Height: {1}" + '\n' + "Name: {2}", this.Weight, this.Height, this.Name);
+        }
+    }
+}
