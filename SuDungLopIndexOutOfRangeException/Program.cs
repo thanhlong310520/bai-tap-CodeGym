40f78b7fb21064189a00e46b6b726a19ded907af
TLong <tilthanhlong@gmail.com>
2023-04-27 08:50:15 +0700

    su dung lop IndexOutOfRangeException

---
+++ SuDungLopIndexOutOfRangeException/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SuDungLopIndexOutOfRangeException
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            try
+            {
+                List<char> characters = new List<char>();
+                characters.InsertRange(0, new char[] { 'a', 'b', 'c', 'd' });
+                int value = characters[6];
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
