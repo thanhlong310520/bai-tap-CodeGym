19eeb9cd8c557a967e1b310cce5c01774d77a7b0
TLong <tilthanhlong@gmail.com>
2023-04-27 08:07:26 +0700

    lop hinh chu nhat

---
+++ HinhChuNhat/Rectangle.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HinhChuNhat
+{
+    class Rectangle
+    {
+        double width;
+        double height;
+        public Rectangle()
+        {
+
+        }
+        public Rectangle(double width, double height)
+        {
+            this.width = width;
+            this.height = height;
+        }
+        public double GetArea()
+        {
+            return width * height;
+        }
+        public double GetPerimeter()
+        {
+            return (width + height) * 2;
+        }
+        public string Display()
+        {
+            return "Rectangle{" + "width=" + width + ", height=" + height + "}";
+        }
+    }
+}
