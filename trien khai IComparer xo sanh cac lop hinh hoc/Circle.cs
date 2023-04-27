0ccbd6709f888ef207bd743f74f063c719856d19
TLong <tilthanhlong@gmail.com>
2023-04-27 08:35:40 +0700

    trien khai interface IComparer de so sanh cac lop hinh hoc

---
+++ trien khai IComparer xo sanh cac lop hinh hoc/Circle.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace trien_khai_IComparer_xo_sanh_cac_lop_hinh_hoc
+{
+    class Circle
+    {
+        double radius;
+        string color;
+        bool filled;
+
+        public Circle(double radius)
+        {
+            this.radius = radius;
+        }
+        public Circle(double radius, string color, bool filled)
+        {
+            this.radius = radius;
+            this.color = color;
+            this.filled = filled;
+        }
+
+        public double Radius { get => radius; set => radius = value; }
+        public string Color { get => color; set => color = value; }
+        public bool Filled { get => filled; set => filled = value; }
+
+        public override string ToString()
+        {
+            return $"hinh tron co ban kinh {radius}";
+
+        }
+    }
+}
---
+++ trien khai IComparer xo sanh cac lop hinh hoc/CircleComparer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Text;
+
+namespace trien_khai_IComparer_xo_sanh_cac_lop_hinh_hoc
+{
+    class CircleComparer : IComparer<Circle>
+    {
+        public int Compare([AllowNull] Circle x, [AllowNull] Circle y)
+        {
+            if (x.Radius > y.Radius) return 1;
+            else if (x.Radius < y.Radius) return -1;
+            else return 0;
+        }
+    }
+}
