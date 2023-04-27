038c4682d390cd7f7dbd0448d49ed6ad25d8dc22
TLong <tilthanhlong@gmail.com>
2023-04-27 08:18:19 +0700

    xay dung lop StopWatch

---
+++ StopWatch/Program.cs
@@ -0,0 +1,44 @@
+﻿
+using System;
+
+namespace StopWatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[100];
+            RandomForArray(ref arr);
+            StopWatch sp = new StopWatch();
+            sp.Start();
+            SelectionSort(arr);
+            sp.Stop();
+            TimeSpan ts =  sp.GetElapsedTime();
+            Console.WriteLine("time span " + ts.Milliseconds);
+
+        }
+        static void SelectionSort(int[]arr)
+        {
+            int temp, smallest;
+            for(int i = 0; i< arr.Length; i++)
+            {
+                smallest = i;
+                for(int j = i + 1; j < arr.Length; j++)
+                {
+                    if (arr[j] < arr[smallest])
+                        smallest = j;
+                }
+                temp = arr[smallest];
+                arr[smallest] = arr[i];
+                arr[i] = temp;
+            }
+        }
+        static void RandomForArray(ref int[] arr)
+        {
+            for(int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = new Random().Next(10000);
+            }
+        }
+    }
+}
---
+++ StopWatch/StopWatch.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StopWatch
+{
+    class StopWatch
+    {
+        DateTime startTime;
+        DateTime endTime;
+        public DateTime GetStartTime()
+        {
+            return startTime;
+        }
+        public DateTime GetEndTime()
+        {
+            return endTime;
+        }
+        public void Start()
+        {
+            startTime = DateTime.Now;
+        }
+        public void Stop()
+        {
+            endTime = DateTime.Now;
+        }
+        public TimeSpan GetElapsedTime()
+        {
+            return endTime.Subtract(startTime);
+        }
+
+
+        
+
+    }
+}
